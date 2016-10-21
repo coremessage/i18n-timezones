701f8c3e9ebd5e743d2a3b9aa039d6a1e9f49d17
cow <cow@zerweb.com>
2016-10-21 17:46:18 +0300

    UTC

--- lib/i18n_timezones/timezone.rb
+++ lib/i18n_timezones/timezone.rb
@@ -1,7 +1,7 @@
 module ActiveSupport
   class TimeZone
     def to_s
-      offset = "(GMT#{formatted_offset})"
+      offset = "(UTC#{formatted_offset})"
       translated_name = I18n.t(name, :scope => :timezones, :default => name, :separator => "\001")
       %(#{offset} #{translated_name})
     end

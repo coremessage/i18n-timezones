fb20e14c3c59ae78d1c1cab154b7540d5d7cebbd
cow <cow@zerweb.com>
2016-10-22 11:47:32 +0300

    Show only UTC offset for timezones with empty names

--- lib/i18n_timezones/timezone.rb
+++ lib/i18n_timezones/timezone.rb
@@ -1,9 +1,9 @@
 module ActiveSupport
   class TimeZone
     def to_s
-      offset = "(UTC#{formatted_offset})"
-      translated_name = I18n.t(name, :scope => :timezones, :default => name, :separator => "\001")
-      %(#{offset} #{translated_name})
+      offset = "UTC#{formatted_offset}"
+      translated_name = I18n.t(name, scope: :timezones, default: name, separator: "\001").presence
+      translated_name ? "(#{offset}) #{translated_name}" : offset
     end
   end
 end

be4c2309e31c713015bac9b5f0b7fb36624f9f10
cow <cow@zerweb.com>
2016-11-29 18:30:27 +0300

    Disable I18n fallbacks

--- lib/i18n_timezones/timezone.rb
+++ lib/i18n_timezones/timezone.rb
@@ -2,7 +2,7 @@ module ActiveSupport
   class TimeZone
     def to_s
       offset = "UTC#{formatted_offset}"
-      translated_name = I18n.t(name, scope: :timezones, default: name, separator: "\001").presence
+      translated_name = I18n.t(name, scope: :timezones, default: name, separator: "\001", fallback: true).presence
       translated_name ? "(#{offset}) #{translated_name}" : offset
     end
   end
